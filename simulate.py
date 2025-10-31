4ca4b1267a41709d2168e5adbf7914a733c32586
mujtahidalam <ashestz@gmail.com>
2025-10-31 22:43:55 +0600

    Add pyrosim generate.py (box.sdf) and load it in simulate.py

--- simulate.py
+++ simulate.py
@@ -4,6 +4,8 @@ import time
 #connect the pysics server with GUI
 physicsClient = p.connect(p.GUI)
 
+p.loadSDF("box.sdf")
+
 #run 1000 simulation steps
 for i in range (1000):
     p.stepSimulation()
