0ed707736ce5fc9fc7e30a10ba2a0c5f53dc0008
mujtahidalam <ashestz@gmail.com>
2025-10-31 13:45:26 +0600

    Adding Simulation Steps and Time

--- simulate.py
+++ simulate.py
@@ -1,4 +1,13 @@
 import pybullet as p
+import time 
 
+#connect the pysics server with GUI
 physicsClient = p.connect(p.GUI)
+
+#run 1000 simulation steps
+for i in range (1000):
+    p.stepSimulation()
+    print("Simulation step:", i)
+    time.sleep(1/60) 
+    
 p.disconnect()
